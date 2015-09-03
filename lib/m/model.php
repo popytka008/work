420b312de10d1c193ff546e8d9cda08ef75e35eb
popytka008 <sasha_1834b@mail.ru>
2015-09-03 20:11:59 +0300

    Определил персональные классы Контролёров и Механиков. Задокументировал классы компонентов. Проверить комментарии классов Контролёров и Механиков.

--- lib/m/model.php
+++ lib/m/model.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once "lib/components/DBMS.php";
-require_once "lib/components/Article.php";
-
 
 class Model {
 
@@ -10,80 +7,7 @@ class Model {
     protected $_error;
 
 
-    public function getArticles($id = null)
-    {
-        $articles = array();
-        $this->_error = "";
-        $server = new DBMS();
-        $result = $server->select($id);
 
-        if (!$result) {
-            $this->_error = $server->error_message . PHP_EOL . $server->error_num;
-        } else {
-            foreach ($result as $arr) {
-                $values = array();
-                foreach ($arr as $k => $v) {
-                    $values[] = $v;
-                }
-                $articles[] = new Article($values);
-            }
-        }
-
-        //echo '<br/>Создание статьи в model->getArticles(): <br/>';
-        //echo var_export($articles) . '<br/>';
-
-        $this->_result = $articles;
-        return $articles;
-    }
-
-
-    public function getArticle($id){
-
-        $article = new Article(array());
-        $this->_error = "";
-        $server = new DBMS();
-        $result = $server->select($id);
-
-        if (!$result) {
-            $this->_error = $server->error_message . PHP_EOL . $server->error_num;
-        } else {
-            $values = array();
-            foreach($result[0] as $k => $v){
-                $values[] = $v;
-            }
-            $article = new Article($values);
-        }
-
-        //echo '<br/>Создание статьи в model->getArticle($id): <br/>';
-        //echo var_export($article).'<br/>';
-
-        $this->_result = $article;
-        return $article;
-    }
-
-
-    public function saveArticle($array)
-    {
-        //echo '<br/>Сохранение статьи в model->saveArticle($params, $id): <br/>';
-        //echo var_export($article).'<br/>';
-
-        $this->_error = "";
-        $server = new DBMS();
-        if(!($this->_result = $server->update(array($array[1], $array[2]), $array[0]))){
-            $this->_error = $server->error_message . PHP_EOL . $server->error_num;
-        }
-    }
-
-    public function deleteArticle($id)  {
-        //echo '<br/>Сохранение статьи в model->saveArticle($params, $id): <br/>';
-        //echo var_export($article).'<br/>';
-
-        $this->_error = "";
-        $server = new DBMS();
-        if(!($this->_result = $server->delete($id))){
-            $this->_error = $server->error_message . PHP_EOL . $server->error_num;
-        }
-    }
     /**
      * @return array | null
      */
@@ -111,7 +35,7 @@ class Model {
 //        File::append('test_file.txt', $str);
 
         return ($_SERVER['REQUEST_METHOD'] === 'POST');
-}
+    }
 
     public function getMethod() {
 //        $str = '/*----------------------- вход и выход в Model()->getMethod() -------------------*/'.PHP_EOL;
